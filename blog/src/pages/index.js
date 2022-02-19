a38f7780e59c8cce1ca13d6329c1a3f6b1e8d3b2
System Administrator <root@192.168.1.68>
2022-02-19 20:54:37 +0100

    Completed Activity 3

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -36,6 +36,9 @@ export const query = graphql`
   allContentfulBlogPost {
     edges {
       node {
+        id
+        title
+        slug
         body {
           childMarkdownRemark {
             excerpt
