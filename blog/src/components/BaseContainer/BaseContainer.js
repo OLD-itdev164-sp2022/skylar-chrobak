4b4ccec4479237a11998affeccbaa7bc713e0381
System Administrator <root@192.168.1.57>
2022-03-22 11:55:45 +0100

    Added heading folder with components and translating current files with new H1

--- blog/src/components/BaseContainer/BaseContainer.js
+++ blog/src/components/BaseContainer/BaseContainer.js
@@ -5,6 +5,7 @@ import { Flex, Box } from 'rebass'
 export const BaseContainer =({children, flex = false, ...rest})=>{
     let Component = flex ? Flex : Box
     return <Component {...rest}>{children}</Component>
+    
 }
 
 BaseContainer.propTypes = {
