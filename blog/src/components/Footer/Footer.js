2a872e31ff1f9157baad58678548ec186b4decc9
System Administrator <root@192.168.1.57>
2022-03-22 12:03:06 +0100

    Adding Footer, Main and section folders and files

---
+++ blog/src/components/Footer/Footer.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import { BaseConatiner } from '../BaseConatiner'
+
+export const Footer = props => <BaseConatiner as='footer' {...props}/>
---
+++ blog/src/components/Section/Section.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import { BaseConatiner } from '../BaseContainer'
+
+export const Section = props => <BaseConatiner as="section" {...props}/>
